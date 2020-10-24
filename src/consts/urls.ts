2cb653b2e21015b864e263fad9dbcd1bd5b7fc44
Sam Piggott <piggott.sam@gmail.com>
2020-10-24 14:35:06 +0100

    fix: analytics

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -1,1 +1,3 @@
 export const PURCHASE_URL = 'https://gum.co/dbWfY';
+export const SUPPORT_EMAIL_ADDRESS = 'codesnap.help@gmail.com';
+export const GITHUB_URL = 'https://github.com/Sarghm/learn-typescript';
